e10a365ee1d0207cc0f9d7d71d618b47b21851f6
emfox55 <emfox5@gmail.com>
2024-11-27 10:08:53 -0800

    Add files via upload
    Updated file with bugfixes, included json

--- Assignment07.py
+++ Assignment07.py
@@ -1,17 +1,21 @@
 # ------------------------------------------------------------------------------------------ #
 # Title: Assignment07
-# Desc: This assignment demonstrates using data classes
-# with structured error handling
+# Desc: This assignment demonstrates using class and objects.
 # Change Log: (Who, When, What)
-#   EFox,11/26/2024,Created Script
+#   EFox,11/19/2024,Created Assignment 06 Script
+#   EFox,11/26/2024,
+#       - Converted dictionary rows to student class objects.
+#       - Added properties and private attributes
+#       - Added inheritance
 # ------------------------------------------------------------------------------------------ #
 import json
 
-# Define the Data Constants
+
+# Data --------------------------------------- #
 MENU: str = '''
 ---- Course Registration Program ----
   Select from the following menu:  
-    1. Register a Student for a Course.
+    1. Register a student for a course.
     2. Show current data.  
     3. Save data to a file.
     4. Exit the program.
@@ -19,96 +23,99 @@ MENU: str = '''
 '''
 FILE_NAME: str = "Enrollments.json"
 
-# Define the Data Variables
+menu_choice: str  # hold the choice made by the user.
 students: list = []  # a table of student data
-menu_choice: str  # Hold the choice made by the user.
 
 
-# TODO Create a Person Class
 class Person:
     """
     A class representing person data.
 
     Properties:
-        student_first_name (str): The student's first name.
-        student_last_name (str): The student's last name.
+        first_name (str): The student's first name.
+        last_name (str): The student's last name.
 
     ChangeLog:
-        - EFox, 11/26/2024: Created the class.
+        EFox,11/26/2024,Created the class.
     """
-    def __init__(self, student_first_name: str = '', student_last_name: str = ''):
-        self.student_first_name = student_first_name
-        self.student_last_name = student_last_name
+
+    def __init__(self, first_name: str = '', last_name: str = ''):
+        self.first_name = first_name
+        self.last_name = last_name
 
     @property
-    def student_first_name(self):
-        return self.student_first_name.title()
+    def first_name(self):
+        return self.__first_name.title()  # formatting code
 
-    @student_first_name.setter
-    def student_first_name(self, value: str):
-        if value.has_numeric() or value == '':
-            self.__student_first_name = value
+    @first_name.setter
+    def first_name(self, value: str):
+        if value.isalpha() or value == "":  # is character or empty string
+            self.__first_name = value
         else:
-            raise ValueError("The first name should not contain numbers or be blank.")
+            raise ValueError("The last name should not contain numbers.")
 
     @property
-    def student_last_name(self):
-        return self.student_last_name.title()
+    def last_name(self):
+        return self.__last_name.title()  # formatting code
 
-    @student_last_name.setter
-    def student_last_name(self, value: str):
-        if value.has_numeric() or value == '':
-            self.__student_last_name = value
+    @last_name.setter
+    def last_name(self, value: str):
+        if value.isalpha() or value == "":  # is character or empty string
+            self.__last_name = value
         else:
-            raise ValueError("The last name should not contain numbers or be blank.")
+            raise ValueError("The last name should not contain numbers.")
 
     def __str__(self):
-        return f'{self.student_first_name},{self.student_last_name}'
+        return f'{self.first_name},{self.last_name}'
 
 
 class Student(Person):
     """
-       A class representing student data.
+    A class representing student data.
 
-       Properties:
-           student_first_name (str): The student's first name.
-           student_last_name (str): The student's last name.
-           course_name (str): The course of the student.
+    Properties:
+        first_name (str): The student's first name.
+        last_name (str): The student's last name.
+        course_name (str): The registered course name of the student.
 
-       ChangeLog: (Who, When, What)
-       EFox, 11/26/2024
-       """
+    ChangeLog: (Who, When, What)
+    EFox,11/26/2024,Created Class
+    """
 
-    def __init__(self, student_first_name: str = '', student_last_name: str = '', course_name: str = ''):
-        super().__init__(student_first_name=student_first_name, student_last_name=student_last_name)
+    def __init__(self, first_name: str = '', last_name: str = '', course_name: str = ''):
+        super().__init__(first_name=first_name, last_name=last_name)
         self.course_name = course_name
-
+        
     @property
     def course_name(self):
-        return self.course_name.title()
+        return self.__course_name
 
     @course_name.setter
     def course_name(self, value: str):
-        self.__course_name = value
+        self.__course_name =  value
 
     def __str__(self):
-        return f'{self.student_first_name},{self.student_last_name},{self.course_name}'
+        return f'{self.first_name},{self.last_name},{self.course_name}'
 
 
 # Processing --------------------------------------- #
 class FileProcessor:
     """
-    A collection of processing layer functions that work with Json files
+    A collection of processing layer functions that work with JSON files
 
     ChangeLog: (Who, When, What)
-    EFox,11/26/2024,Created Class
+    EFox,11/19/2024,Created class
+    EFox,11/26/2024,Converted code to use student objects instead of dictionaries
     """
+
     @staticmethod
     def read_data_from_file(file_name: str, student_data: list):
-        """ This function reads data from a json file and loads it into a list of dictionary rows
+        """
+        A function to read the data from a specified JSON file, and into list.
 
         ChangeLog: (Who, When, What)
-        EFox,11/26/2024,Created function
+        EFox,11/19/2024,Created Function
+        EFox,11/26/2024,Converted list of dictionaries to list of student objects
 
         :param file_name: string data with name of file to read from
         :param student_data: list of dictionary rows to be filled with file data
@@ -118,75 +125,83 @@ class FileProcessor:
 
         try:
             file = open(file_name, "r")
-            list_of_dictionary_data = json.load(file)
+
+            list_of_dictionary_data = json.load(file)  # the load function returns a list of dictionary rows.
             for student in list_of_dictionary_data:  # Convert the list of dictionary rows into Student objects
-                student_object: Student = Student(student_first_name=student["FirstName"],
-                                                  student_last_name= student["LastName"],
+                student_object: Student = Student(first_name=student["FirstName"],
+                                                  last_name= student["LastName"],
                                                   course_name=student["CourseName"])
                 student_data.append(student_object)
-            file.close()
-        except Exception as e:
-            IO.output_error_messages(message="Error: There was a problem with reading the file.", error=e)
 
+            file.close()
+        except FileNotFoundError as e:
+            IO.output_error_messages(message = 'File must exist before running this script!', error = e)
+        except Exception as e:
+            IO.output_error_messages(message = 'There was a non-specific error', error = e)
         finally:
             if file.closed == False:
                 file.close()
         return student_data
 
     @staticmethod
-    def write_data_to_file(file_name: str, student_data: list):
-        """ This function writes data to a json file with data from a list of dictionary rows
+    def write_data_to_file(file_name: str, student_data:list):
+        """
+        A function to write the data to a specified JSON file from a list
 
         ChangeLog: (Who, When, What)
-        EFox,11/26/2024,Created function
+        EFox,11/19/2024,Created Function
+        EFox,11/26/2024,Converted code to use student objects instead of dictionaries
 
         :param file_name: string data with name of file to write to
         :param student_data: list of dictionary rows to be writen to the file
 
         :return: None
         """
-
         try:
             list_of_dictionary_data: list = []
             for student in student_data:  # Convert List of Student objects to list of dictionary rows.
                 student_json: dict \
-                    = {"FirstName": student.student_first_name, "LastName": student.student_last_name, "CourseName": student.course_name}
+                    = {"FirstName": student.first_name, "LastName": student.last_name, "CourseName": student.course_name}
                 list_of_dictionary_data.append(student_json)
 
-            file = open(file_name, "w")
-            json.dump(list_of_dictionary_data, file)
+            file = open(FILE_NAME,'w')
+            json.dump(list_of_dictionary_data,file)
             file.close()
-            IO.output_student_and_course_names(student_data=student_data)
+        except TypeError as e:
+            IO.output_error_messages(message = 'Are the file contents in a valid JSON format?', error= e)
         except Exception as e:
-            message = "Error: There was a problem with writing to the file.\n"
-            message += "Please check that the file is not open by another program."
-            IO.output_error_messages(message=message,error=e)
+            IO.output_error_messages(message = 'There was a non-specific error', error = e)
         finally:
             if file.closed == False:
                 file.close()
+
+        print('The following data was saved to file!')
+        IO.output_student_courses(student_data = student_data)
 
 
 # Presentation --------------------------------------- #
 class IO:
     """
-    A collection of presentation layer functions that manage user input and output
+    A collection of presentation layer functions that regulate data shown
+    to and collected from the user
 
-    ChangeLog: (Who, When, What)
-    EFox,11.26.2024,Created Class
+    ChangeLog: (Who,When,What)
+    EFox,11/19/2024,Created Class
+    EFox,11/26/2024,Converted methods to use student objects instead of dictionaries
     """
 
     @staticmethod
     def output_error_messages(message: str, error: Exception = None):
-        """ This function displays a custom error message to the user
+        """ This function displays a custom error messages to the user
 
-        ChangeLog: (Who, When, What)
-        EFox,11/26/2024,Created function
+         ChangeLog: (Who, When, What)
+         EFox,11/19/2024,Created Function
 
-        :param message: string with message data to display
-        :param error: Exception object with technical message to display
+         :param message: string with message data to display
+         :param error: Exception object with technical message to display
 
-        :return: None
-        """
+         :return: None
+         """
         print(message, end="\n\n")
         if error is not None:
             print("-- Technical Error Message -- ")
@@ -194,61 +209,57 @@ class IO:
 
     @staticmethod
     def output_menu(menu: str):
-        """ This function displays the menu of choices to the user
+        """ This function displays a menu of choices to the user
 
         ChangeLog: (Who, When, What)
-        EFox,11/26/2024,Created function
-
+        EFox,11/19/2024,Created Function
 
         :return: None
         """
-        print()  # Adding extra space to make it look nicer.
-        print(menu)
-        print()  # Adding extra space to make it look nicer.
+        print()
+        print(MENU)
+        print()
 
     @staticmethod
-    def input_menu_choice():
+    def input_menu_choice(menu: str) -> str:
         """ This function gets a menu choice from the user
 
         ChangeLog: (Who, When, What)
-        EFox,11/26/2024,Created function
+        EFox,11/19/2024,Created function
 
-        :return: string with the users choice
+        :return: string with the user's choice
         """
-        choice = "0"
-        try:
-            choice = input("Enter your menu choice number: ")
-            if choice not in ("1","2","3","4"):  # Note these are strings
-                raise Exception("Please, choose only 1, 2, 3, or 4")
-        except Exception as e:
-            IO.output_error_messages(e.__str__())  # Not passing e to avoid the technical message
-
-        return choice
+        menu_choice = input('What would you like to do: ')
+        while menu_choice not in ['1','2','3','4']:
+            IO.output_error_messages('Please enter a number between 1 and 4.')
+            menu_choice = input("What would you like to do: ")
+        return menu_choice
 
     @staticmethod
-    def output_student_and_course_names(student_data: list):
-        """ This function displays the student and course names to the user
+    def output_student_courses(student_data: list):
+        """ This function displays the student data to the user
 
         ChangeLog: (Who, When, What)
-        EFox,11/26/2024,Created function
+        EFox,11/19/2024,Created function
+        EFox,11/26/2024,Converted code to use student objects instead of dictionaries
 
-        :param student_data: list of dictionary rows to be displayed
+        :param student_data: list of student object data to be displayed
 
         :return: None
         """
-
         print("-" * 50)
         for student in student_data:
-            print(f'Student {student["FirstName"]} '
-                  f'{student["LastName"]} is enrolled in {student["CourseName"]}')
+            print(f'Student {student.first_name} '
+                  f'{student.last_name} is enrolled in {student.course_name}')
         print("-" * 50)
 
     @staticmethod
     def input_student_data(student_data: list):
-        """ This function gets the student's first name and last name, with a course name from the user
+        """ This function gets the first name, last name, and course name from the user
 
         ChangeLog: (Who, When, What)
-        EFox,11/26/2024,Created function
+        EFox,11/19/2024,Created Function
+        EFox,11/26/2024,Converted code to use student objects instead of dictionaries
 
         :param student_data: list of dictionary rows to be filled with input data
 
@@ -256,68 +267,46 @@ class IO:
         """
 
         try:
-            # Input the data
             student = Student()
-            student.student_first_name = input("What is the student's first name? ")
-            student.student_last_name = input("What is the student's last name? ")
-            student.course_name = float(input("What is the student's course name? "))
+            student.first_name = input('Enter the student\'s first name ')
+            student.last_name = input('Enter the student\'s last name ')
+            student.course_name = input('Enter the name of the course ')
             student_data.append(student)
-            print()
-            print(f"You have registered {student_first_name} {student_last_name} for {course_name}.")
+            print(f"You have registered {student.first_name} {student.last_name} for {student.course_name}.")
         except ValueError as e:
-            IO.output_error_messages(message="One of the values was the correct type of data!", error=e)
+            IO.output_error_messages(message = 'The value is not valid.', error = e)
         except Exception as e:
-            IO.output_error_messages(message="Error: There was a problem with your entered data.", error=e)
-        return student_data
+            IO.output_error_messages(message = 'Error: There was a problem with your entered data.', error = e)
+# End of class and function definitions
 
-    @staticmethod
-    def has_numeric(input_string: str):
-        """ This function loops through each character in a string and returns a boolean value of True
-        if any character is numeric
-
-        ChangeLog: (Who, When, What)
-        EFox,11/26/2024,Created Function
-
-        :return: Boolean
-        """
-        for char in input_string:
-            if char.isnumeric():
-                return True
-        return False
-
-# Start of main body
 
 # When the program starts, read the file data into a list of lists (table)
-# Extract the data from the file
-students = FileProcessor.read_data_from_file(file_name=FILE_NAME, student_data=students)
+students = FileProcessor.read_data_from_file(file_name = FILE_NAME, student_data = students)
 
 # Present and Process the data
-while (True):
-
+while True:
     # Present the menu of choices
-    IO.output_menu(menu=MENU)
+    print(MENU)
+    # Store user menu choice
+    menu_choice = IO.input_menu_choice(menu = MENU)
 
-    menu_choice = IO.input_menu_choice()
-
-    # Input user data
-    if menu_choice == "1":  # This will not work if it is an integer!
-        students = IO.input_student_data(student_data=students)
+    if menu_choice == '1':
+        # Input user data
+        IO.input_student_data(student_data = students)
         continue
 
-    # Present the current data
-    elif menu_choice == "2":
-        IO.output_student_and_course_names(students)
+    elif menu_choice == '2':
+        # Present the current data
+        IO.output_student_courses(student_data = students)
         continue
 
-    # Save the data to a file
-    elif menu_choice == "3":
-        FileProcessor.write_data_to_file(file_name=FILE_NAME, student_data=students)
+    elif menu_choice == '3':
+        # Save the data to a file
+        FileProcessor.write_data_to_file(file_name = FILE_NAME, student_data = students)
         continue
 
-    # Stop the loop
-    elif menu_choice == "4":
-        break  # out of the loop
-    else:
-        print("Please only choose option 1, 2, or 3")
+    elif menu_choice == '4':
+        # Stop the loop
+        break
 
 print("Program Ended")
